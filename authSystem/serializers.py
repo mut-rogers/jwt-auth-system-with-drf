3d0b62c8f49c595302abde6ef80cee4b7f09d1e0
Rogers Macharia <rogersmacharia77@gmail.com>
2022-06-22 11:40:26 +0300

    created a UserRegistrationSerializer for registering or updating users in the system

---
+++ authSystem/serializers.py
@@ -0,0 +1,36 @@
+from django.contrib.auth.models import User 
+from rest_framework import serializers, validators
+
+
+class UserRegistrationSerializer(serializers.ModelSerializer):
+    """
+    This model serializers is used to Register/Update users 
+    """
+    class Meta: 
+        model = User 
+        fields = ["email", "username", "password"] 
+        extra_kwargs = {
+            "password": {
+                "write_only": True
+            },
+
+            # Ensure email is required and unique
+            "email": {
+                "required": True,
+                "validators": validators.UniqueValidator(
+                    User.objects.all(),
+                    message="A user with that email already exists"
+                )
+            }
+        }
+
+    def create(self, validated_data):
+        """
+        Extra checking to ensure everything works well
+        """
+        password = validated_data.pop("password")
+        instance = self.Meta.model(**validated_data) 
+        if password is not None:
+            instance.set_password(password)
+        instance.save()
+        return instance
