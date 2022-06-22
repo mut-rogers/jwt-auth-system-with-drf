51016b74e4b3e0fbe131d66029c7e1c97ac9558b
Rogers Macharia <rogersmacharia77@gmail.com>
2022-06-22 11:03:15 +0300

    setup settings.py

--- authSystem/settings.py
+++ authSystem/settings.py
@@ -1,5 +1,6 @@
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -9,7 +10,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ncjhfgcyq&+))gfuwp7j*5)!=9ev+3rq&0x$5%d%u(%j+k9+u*'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
