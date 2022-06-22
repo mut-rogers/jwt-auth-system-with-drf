4003765db97dcf821e058b44cb0cd26abefa0fdf
Rogers Macharia <rogersmacharia77@gmail.com>
2022-06-22 12:11:43 +0300

    completed registration capabilities of the system

--- accounts/urls.py
+++ accounts/urls.py
@@ -6,4 +6,5 @@ from rest_framework_simplejwt import views as jwt_views
 urlpatterns = [
     path("login/", jwt_views.TokenObtainPairView.as_view(), name="login"),
     path("refresh/", jwt_views.TokenRefreshView.as_view(), name="refresh"),
+    path("register/", views.UserRegistrationAPIView.as_view(), name="register")
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from rest_framework import status, permissions, generics
+from rest_framework.response import Response 
+from rest_framework.views import APIView 
+from django.contrib.auth.models import User
+from . import serializers
 
-# Create your views here.
+
+class UserRegistrationAPIView(APIView):
+    """
+    An APIView to register new users into the systems
+    """ 
+    # Authentication is not required to access this view 
+
+    # Defining a serializer class 
+    serializer_class = serializers.UserRegistrationSerializer
+
+    def post(self, request) -> Response: 
+        serializer = self.serializer_class(data=self.request.data) 
+        if serializer.is_valid():
+            return Response(serializer.data, status=status.HTTP_201_CREATED) 
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
