8e52bcebe94df576242a011404778d29831af04e
Muthoni Rogers <macharia_rogers@outlook.com>
2022-06-25 00:56:56 +0300

    set up the urls file for blogs app

--- authSystem/urls.py
+++ authSystem/urls.py
@@ -5,4 +5,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("api/auth/", include("accounts.urls")),
+    path("api/blog/", include("blog.urls")),
 ]
